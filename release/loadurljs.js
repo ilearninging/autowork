d161c0355783ced8fa3819bc055521c172986eb5
ilearninging <233551139@qq.com>
2015-11-17 20:05:27 +0800

    add load url js

---
+++ release/loadurljs.js
@@ -0,0 +1,9 @@
+/*
+ * @name load remote jquery js  and do something
+ * @description load remote js like jquery and do something
+ * @author ilearninging
+ *
+ */
+load("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/js/lib/jquery-1.10.2_d88366fd.js",function(){
+autowork.log($);
+});
