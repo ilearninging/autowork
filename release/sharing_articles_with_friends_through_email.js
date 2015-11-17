6970164987a3ba655a96e9a53a12d2da0bd62566
ilearninging <233551139@qq.com>
2015-11-17 13:37:10 +0800

    sharing

---
+++ release/sharing_articles_with_friends_through_email.js
@@ -0,0 +1,11 @@
+/*
+ * @name Sharing articles with friends through mail
+ * @description the task need both Drafts and AutoWork working together to send a mail as html with copied URL
+ * @author ilearninging
+ *
+ */
+function callback(text){
+var clip=getClip();
+openURL("drafts://x-callback-url/create?text="+text+"%0D%20%5BLink%5D%28"+clip+"%29%20%0D%0Dhtnawsaj&action={{Share Link}}&x-success={{autowork://}}");
+}
+prompt("text",callback);
