975d393b5aef74a0a74347620e0be3cb89751133
ilearninging <233551139@qq.com>
2015-11-18 11:57:19 +0800

    save to album

--- help/saveToAlbum.js
+++ help/saveToAlbum.js
@@ -1,6 +1,6 @@
 /*
- * @name changeImage(image,compressionQuality)
- * @description this method returns the specified image in JPEG format. The quality of the resulting JPEG image,expressed as a value from 0.0 to 1.0. the value 0.0 represents the maximum compression(or lowest quality) while the value 1.0 represents the least compression (or best quality).
+ * @name saveToAlbum(image)
+ * @description this method save image to album
  * @author AutoWork
  *
  */
